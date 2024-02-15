d5a65e04037193c64095762e9d922f04ad58b26b
Janybek <messenger-by-hack@gmail.com>
2024-02-15 22:22:25 +0600

    cors

--- client/next.config.js
+++ client/next.config.js
@@ -16,20 +16,20 @@ const nextConfig = {
     loader: 'default',
     path: 'https://messenger-by-hack-server.vercel.app/static/',
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
